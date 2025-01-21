ecddbf204eb5de1ded2bc5327e218c148476bb42
mlgepicbanana69 <MlgEpicBanana69@gmail.com>
2025-01-21 22:32:19 +0200

    3.18 complete

--- part3/personserver/index.js
+++ part3/personserver/index.js
@@ -91,6 +91,22 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(id, person, { new: true, runValidators: true })
+        .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -103,6 +119,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: "Request does not adhere to required parameters' validations"})
     }
 
     next(error)
