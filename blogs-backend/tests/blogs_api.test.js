78ee4b7fec703cdf18dd11397b983140e4ffbdc4
mlgepicbanana69 <MlgEpicBanana69@gmail.com>
2025-01-28 21:48:49 +0200

    4.11 complete

--- blogs-backend/tests/blogs_api.test.js
+++ blogs-backend/tests/blogs_api.test.js
@@ -20,6 +20,18 @@ test('all blogs are being shown', async () => {
   assert.strictEqual(response.body.length, helper.initalBlogs.length)
 })
 
+test('blogs have id but not _id and __v', async () => {
+  const blogsInDb = await helper.blogsInDb()
+
+  assert(!blogsInDb.some(blog =>
+    blog._id !== undefined
+    ||
+    blog.__v !== undefined
+    ||
+    blog.id === undefined
+  ))
+})
+
 test('can create valid blog', async () => {
   const blogsAtStart = await helper.blogsInDb()
 
@@ -39,6 +51,26 @@ test('can create valid blog', async () => {
   const blogsAtEnd = await helper.blogsInDb()
   assert.deepEqual(blogsAtEnd.length, blogsAtStart.length + 1)
   assert(blogsAtEnd.some(blog => blog.title === 'How to make a good toast'))
+})
+
+test('created blog with no likes has default of 0 likes', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+
+  const blogWithNoLikes = {
+    title: '600pp easy tutorial for beginners',
+    author: 'My Angel Aerora',
+    url: 'https://epicgaming.ppy.sh'
+  }
+
+  await api
+    .post('/api/blogs/')
+    .send(blogWithNoLikes)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+  assert(blogsAtEnd.some(blog => blog.title === '600pp easy tutorial for beginners' && blog.likes === 0))
 })
 
 beforeEach(async () => {
